0f2f8d5814bdafd33bf65d10516ad5bde61698c2
maks <maks>
2018-10-21 19:50:18 +0300

    прошу допомогти із воконанням домашньої роботи

---
+++ src/human/Human.java
@@ -0,0 +1,49 @@
+package human;
+
+import java.time.LocalDate;
+
+public class Human {
+	private String name;
+	private String surName;
+	private LocalDate date;
+	private String position;
+	private double salary;
+	public Human(String name, String surName, LocalDate date, String position, double salary) {
+		super();
+		this.name = name;
+		this.surName = surName;
+		this.date = date;
+		this.position = position;
+		this.salary = salary;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurName() {
+		return surName;
+	}
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}	
+}
---
+++ src/test/TestWrite.java
@@ -0,0 +1,30 @@
+package test;
+
+import java.io.*;
+import java.time.LocalDate;
+
+import db.HumanDAO;
+import human.Human;
+
+public class TestWrite {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		HumanDAO humanDAO = new HumanDAO(new File("/home/maks/SaveGame/test.txt"));
+	
+		//записує поверх існуючих данних
+		
+		Human human[] = new Human[3];
+		human[0] = new Human("Sasha", "Grey",LocalDate.of(1988, 03, 14), "actress", 1000);
+		human[1] = new Human("Jesse", "Jane", LocalDate.of(1980, 07, 16), "actress", 1000);
+		human[2] = new Human("Gianna", "Mihcaels", LocalDate.of(1983, 06, 06), "actress", 1000);
+			try {
+				for (Human h : human)
+				humanDAO.write(h);
+				System.out.println("write - ok");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			
+	}
+}
